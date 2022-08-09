2e779f1237a6899beaee752a1c402d3c95f7e5d6
D-BINH\dybnh <duybinh03.dev@gmail.com>
2022-08-09 09:23:09 +0700

    second commit

--- Su-dung-vong-lap-while/src/sdWhile1.js
+++ Su-dung-vong-lap-while/src/sdWhile1.js
@@ -1,13 +1,28 @@
-let number = parseInt(prompt("Moi nhap so "))
-let sum = 0
-while(number !== -1){
+// Bai1
 
-     number = parseInt(prompt("Moi nhap so"))
-     sum += number
 
-     alert("Tong cac so la : " + sum)
+// let number = parseInt(prompt("Moi nhap so "))
+// let sum = 0
+// while(number !== -1){
 
-}
+//      number = parseInt(prompt("Moi nhap so"))
+//      sum += number
+
+//      alert("Tong cac so la : " + sum)
+
+// }
+
+
+
+// Bai2
+
+// let i = 1;
+
+// while (i < 100) {
+//     // phân thân vòng lặp
+//     document.write("<hr width = " + i + "%>");
+//     i++;
+// }
 
 
 
